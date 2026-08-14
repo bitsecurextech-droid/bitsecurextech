961415cf582dce89f37f572f9769663c954d26df
bitsecurextech-droid <bitsecurex.tech@gmail.com>
2026-08-14 03:57:43 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from './lib/router';
+import { AuthProvider } from './lib/auth';
+import { ThemeProvider } from './lib/useReveal';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <AuthProvider>
+        <RouterProvider>
+          <App />
+        </RouterProvider>
+      </AuthProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
